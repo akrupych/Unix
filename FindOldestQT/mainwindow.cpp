d913fbf270ea3699947ed44697dddb826e778cc9
Krupych Andriy <andriy.krupych@gmail.com>
2012-03-21 15:35:38 +0200

    qt updated

--- FindOldestQT/mainwindow.cpp
+++ FindOldestQT/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "dirfinder.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -17,11 +18,17 @@ MainWindow::~MainWindow()
 void MainWindow::on_pushButton_clicked()
 {
     if (ui->lineEdit->text().isEmpty()) return;
-    ui->label_2->setText("Working...");
     DirFinder finder;
     string arg = ui->lineEdit->text().toStdString();
     string result = finder.the_oldest_subdir(arg);
     if (result.empty()) result = "Sorry, no subdirectories can be found";
     ui->lineEdit_2->setText(QString::fromStdString(result));
-    ui->label_2->setText("Ready");
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QFileDialog* dialog = new QFileDialog();
+    dialog->setFileMode(QFileDialog::DirectoryOnly);
+    if (dialog->exec())
+        ui->lineEdit->setText(dialog->directory().path());
 }
